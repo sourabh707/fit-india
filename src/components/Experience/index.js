d75fde8ed080346d4a9eb74c842d418d1aee4792
sourabh <85352464+sourabh707@users.noreply.github.com>
2023-05-24 22:15:16 +0530

    experience done

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,46 +1,94 @@
-import React from 'react'
-import './index.css'
+import React, { useState } from 'react';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Modal, Button } from 'react-bootstrap';
 
 const Experience = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    height: '',
+    startingWeight: '',
+    presentWeight: '',
+    comments: '',
+  });
+
+  const [showModal, setShowModal] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setShowModal(true);
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      height: '',
+      startingWeight: '',
+      presentWeight: '',
+      comments: '',
+    });
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };  
+
   return (
     <div className='Experience-container'>
       {/* <h1>Share Your Experience</h1> */}
-      <form className="row g-3">
+      <form className="row g-3" onSubmit={handleSubmit}>
         <h2>Share your Experience</h2>
         <div className="col-md-6 text-start">
           <label className="form-label text-start">First Name</label>
-          <input type="text" className="form-control" placeholder='First Name'required />
+          <input type="text" className="form-control" placeholder='First Name' required name='firstName' value={formData.firstName} onChange={handleChange} />
         </div>
         <div className="col-md-6 text-start">
           <label className="form-label">Last Name</label>
-          <input type="text" className="form-control" placeholder='Last Name' required/>
+          <input type="text" className="form-control" placeholder='Last Name' required name='lastName' value={formData.lastName} onChange={handleChange} />
         </div>
         <div className="col-md-9 text-start">
           <label className="form-label">Email address</label>
-          <input type="email" className="form-control" placeholder='name@example.com' required/>
+          <input type="email" className="form-control" placeholder='name@example.com' required name='email' value={formData.email} onChange={handleChange} />
         </div>
         <div className="col-md-3 text-start">
           <label className="form-label">Height</label>
-          <input type="number" className="form-control" placeholder='Height (in cm)' required/>
+          <input type="number" className="form-control" placeholder='Height (in cm)' required name='height' value={formData.height} onChange={handleChange} />
         </div>
         <div className="col-md-6 text-start">
           <label className="form-label">Staring Height</label>
-          <input type="number" className="form-control" placeholder='Weight (in kg)' required/>
+          <input type="number" className="form-control" placeholder='Weight (in kg)' required name='startingWeight' value={formData.startingWeight} onChange={handleChange} />
         </div>
         <div className="col-md-6 text-start">
           <label className="form-label">Present Weight</label>
-          <input type="number" className="form-control" placeholder=' Weight (in kg)' required />
+          <input type="number" className="form-control" placeholder=' Weight (in kg)' required name='presentWeight' value={formData.presentWeight} onChange={handleChange} />
         </div>
         <div className="col-md-12 text-start">
-          <label  className="form-label">Suggestions & Comments</label>
-          <textarea className="form-control"  rows="3" required/>
+          <label className="form-label">Suggestions & Comments</label>
+          <textarea className="form-control" rows="3" required name='comments' value={formData.comments} onChange={handleChange} />
         </div>
         <div className="col">
           <button type="submit" className="btn btn-dark">Submit</button>
         </div>
       </form>
 
+
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Thanks for your suggestions!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>We will surely consider them.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 };
--- src/components/Nutrition/index.js
+++ src/components/Nutrition/index.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import axios from 'axios';
+// import axios from 'axios';
 import './index.css'
 function Nutrition ()  {
-  const [data, setData] = React.useState([]);
+  // const [data, setData] = React.useState([]);
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition', {
-          headers: {
-            'X-RapidAPI-Key': 'bf9a0a9b84msh0ccfc969a853169p15e576jsndf7b88f888e0',
-            'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
-          }
-        },{
-          params: {
-            query: '1lb brisket with fries'
-          },
-        });
-        // setData(response.data);
-      } catch (error) {
-        console.log(error, 'hsdfhsd');
-      }
-    };
-    fetchData();
-  }, []);
+  // React.useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition', {
+  //         headers: {
+  //           'X-RapidAPI-Key': 'bf9a0a9b84msh0ccfc969a853169p15e576jsndf7b88f888e0',
+  //           'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
+  //         }
+  //       },{
+  //         params: {
+  //           query: '1lb brisket with fries'
+  //         },
+  //       });
+  //       // setData(response.data);
+  //     } catch (error) {
+  //       console.log(error, 'hsdfhsd');
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
   return (
     <div>
       
