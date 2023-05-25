75bef61b9276a429958a7cd72fc355f15fc5624f
sourabh <85352464+sourabh707@users.noreply.github.com>
2023-05-25 13:30:47 +0530

    done

--- src/components/Nutrition/index.js
+++ src/components/Nutrition/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
 
-import './index.css'
+import './index.css';
 
 function NutritionCard({ nutrition }) {
   return (
@@ -13,27 +13,27 @@ function NutritionCard({ nutrition }) {
         <p className="card-text">Protein: {nutrition.protein_g}</p>
         <p className="card-text">Fat: {nutrition.fat_total_g}</p>
         <p className="card-text">Sugar: {nutrition.sugar_g}</p>
-
       </div>
     </div>
   );
 }
 
-
-function Nutrition ()  {
+function Nutrition() {
   const [data, setData] = React.useState([]);
   const [error, setError] = React.useState(null);
   const [inputValue, setInputValue] = React.useState('');
-  
+  const [isSearchPerformed, setIsSearchPerformed] = React.useState(false);
+
   const fetchData = async () => {
     try {
       const url = `https://api.api-ninjas.com/v1/nutrition?query=${inputValue}`;
       const headers = {
-        'X-Api-Key': 'crhvbQXiAxGOczByUzQlGJwfXR5wjMufBdMvjCq7'
+        'X-Api-Key': 'crhvbQXiAxGOczByUzQlGJwfXR5wjMufBdMvjCq7',
       };
 
       const response = await axios.get(url, { headers });
       setData(response.data);
+      setIsSearchPerformed(true);
       console.log(response.data, 'success');
     } catch (error) {
       console.log(error, 'error');
@@ -41,36 +41,46 @@ function Nutrition ()  {
     }
   };
 
-  React.useEffect(() => {
-    {
-      fetchData();
-    }
-  }, []);
-
   const handleSearch = () => {
     fetchData();
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
+  };
+
   if (error) return `Error: ${error.message}`;
+
   return (
-    <div className='Nutrition-container'>
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1 text-start" className="form-label mb-3">
-          <h1>Food Item Name</h1>
-        </label>
-        <input type="text" className="form-control" list="defaultValues"placeholder="Enter food items name" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-        <button className="btn btn-primary mt-3" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
-      {data.length === 0 && <p>No data found. Please try another word.</p>}
-      {data && <div className="nutrition-card-container">
-        {data.map((nutrition) => (
-          <NutritionCard key={nutrition.name} nutrition={nutrition} />
-        ))}
-      </div>}
+    <div className="Nutrition-container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1 text-start" className="form-label mb-3">
+            <h1>Food Item Name</h1>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter food item name"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="btn btn-primary mt-3" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+      {isSearchPerformed && data.length === 0 && <p>No data found. Please try another word.</p>}
+      {data.length > 0 && (
+        <div className="nutrition-card-container">
+          {data.map((nutrition) => (
+            <NutritionCard key={nutrition.name} nutrition={nutrition} />
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Nutrition
+export default Nutrition;
